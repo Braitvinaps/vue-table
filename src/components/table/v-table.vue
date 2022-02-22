cb5469a4b9de086f9377fd0e363ce00fb9892c42
Alexy <britvinaps@gmail.com>
2022-02-22 12:36:34 +0300

    first commit

---
+++ src/components/table/v-table.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="v-table">
+      <div class="v-table__header">
+          <p>Username</p>
+          <p>Email</p>
+          <p>Rating</p>
+          <p>Ragistration date</p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "v-table",
+    props: {
+        users_data: {
+            type: Array,
+            default: () => {
+                return []
+            }
+        }
+    },
+    data() {
+        return {}
+    },
+    computed: {}
+}
+</script>
+
+<style>
+    .v-table {
+        max-width: 900px;
+        margin: 0 auto;
+    }
+    .v-table__header {
+        display: flex;
+        justify-content: space-around;
+    }
+</style>
