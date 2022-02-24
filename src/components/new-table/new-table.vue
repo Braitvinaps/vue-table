f1598b930657d17c2e7d87a844811ef3e8ca2c15
Alexy <britvinaps@gmail.com>
2022-02-24 22:25:42 +0300

    4

--- src/components/new-table/new-table.vue
+++ src/components/new-table/new-table.vue
@@ -4,7 +4,7 @@
       <input
         type="text"
         class="search"
-        placeholder="Search username or email"
+        placeholder="Search by username or email"
         v-model="searchUser"
       />
     </div>
@@ -117,7 +117,8 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+
 .container {
   margin-top: 50px;
 }
