43b7b21f929fa9cae7bcebc43cbf6910600237cd
Alexy <britvinaps@gmail.com>
2022-02-27 18:04:31 +0300

    6

--- src/App.vue
+++ src/App.vue
@@ -33,6 +33,10 @@ export default {
 
 <style>
 #app {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
